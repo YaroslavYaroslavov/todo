211c8724636df802bef6666563d249ae626bb5ee
Yaroslav <tvoibaget@mail.ru>
2022-09-09 16:55:12 +0300

    divide renderItems to renderItems and createTaskElement

--- main.js
+++ main.js
@@ -12,63 +12,62 @@ document.addEventListener('DOMContentLoaded', () => {
     renderItems()
 })
 
+const createTaskElement = (item) => {
+
+    const taskListWrapper = document.createElement('div'),
+        line = document.createElement('div'),
+        stringWrap = document.createElement('div'),
+        nameTask = document.createElement('div'),
+        nameResponsible = document.createElement('div'),
+        levelPriority = document.createElement('div'),
+        checkBtn = document.createElement('button');
+    let priority = levelPriority.textContent;
+
+    taskListWrapper.classList.add('taskListWrapper')
+    checkBtn.classList.add('checkBtn')
+    stringWrap.classList.add('stringWrap')
+    line.classList.add('line')
+    nameTask.classList.add('nameTask')
+    levelPriority.classList.add('levelPriority')
+    nameResponsible.classList.add('nameResponsible')
+
+    nameTask.textContent = item.name
+    nameResponsible.textContent = item.responsible
+
+    levelPriority.textContent = item.priority
+    priority = levelPriority.textContent
+    levelPriority.classList.add(`priority${priority}`)
 
 
-function removeTask(a) {
+    taskCount = taskList.childElementCount - 1;
+    taskListWrapper.id = `${taskCount}`
+    checkBtn.addEventListener('click', () => removeTask(Number(taskListWrapper.id)))
+    taskListWrapper.append(nameTask)
+    taskListWrapper.append(nameResponsible)
+    taskListWrapper.append(levelPriority)
+    taskListWrapper.append(checkBtn)
+
+    stringWrap.append(taskListWrapper)
+    stringWrap.append(line)
+    return stringWrap
+}
+
+
+const removeTask = (a) => {
     tasks.splice(a, 1);
     localStorage.setItem('tasksStorage', JSON.stringify(tasks))
     renderItems();
+
 }
 
 function renderItems() {
-
-    taskList.textContent = '';
+    taskList.textContent = ''; // бля не бей я в процессе
     tasks.forEach((item) => {
-        const taskListWrapper = document.createElement('div')
-        const line = document.createElement('div')
-        const stringWrap = document.createElement('div')
-        stringWrap.classList.add('stringWrap')
-        line.classList.add('line')
 
+        stringWrap = createTaskElement(item)
 
-        const nameTask = document.createElement('div')
-        nameTask.classList.add('nameTask')
-
-        const nameResponsible = document.createElement('div')
-        nameResponsible.classList.add('nameResponsible')
-        const levelPriority = document.createElement('div')
-        levelPriority.classList.add('levelPriority')
-        const checkBtn = document.createElement('button')
-
-        taskListWrapper.classList.add('taskListWrapper')
-
-
-
-        checkBtn.classList.add('checkBtn')
-
-
-        nameTask.textContent = item.name
-        nameResponsible.textContent = item.responsible
-        levelPriority.textContent = item.priority
-        const priority = levelPriority.textContent
-        levelPriority.classList.add(`priority${priority}`)
-
-        taskListWrapper.append(nameTask)
-        taskListWrapper.append(nameResponsible)
-        taskListWrapper.append(levelPriority)
-        taskListWrapper.append(checkBtn)
-            // taskListWrapper.append(line)
-        stringWrap.append(taskListWrapper)
-        stringWrap.append(line)
         taskList.append(stringWrap)
-
-        taskCount = taskList.childElementCount - 1;
-        taskListWrapper.id = `${taskCount}`
-        checkBtn.addEventListener('click', () => removeTask(Number(taskListWrapper.id)))
-
     })
-
-
 }
 
 function addTask() {
