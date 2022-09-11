e7fa668577928b1aa5b9a5eb091b1f2a6d0d544b
Yaroslav <tvoibaget@mail.ru>
2022-09-11 14:10:42 +0300

    fixed localStorage

--- main.js
+++ main.js
@@ -9,88 +9,90 @@ let taskCount = taskList.childElementCount;
 let tasks = []
 
 document.addEventListener('DOMContentLoaded', () => {
-    tasks = JSON.parse(localStorage.getItem('tasksStorage'))
-    renderItems()
-})
+    if (localStorage.getItem('tasksStorage') !== null) {
+        tasks = JSON.parse(localStorage.getItem('tasksStorage'))
+    } else(
+        localStorage.setItem('tasksStorage', tasks)
+    )
+    const createTaskElement = (item) => {
+        const taskListWrapper = document.createElement('div'),
+            line = document.createElement('div'),
+            stringWrap = document.createElement('div'),
+            nameTask = document.createElement('div'),
+            nameResponsible = document.createElement('div'),
+            levelPriority = document.createElement('div'),
+            checkBtn = document.createElement('button');
+        let priority = levelPriority.textContent;
 
-const createTaskElement = (item) => {
-    const taskListWrapper = document.createElement('div'),
-        line = document.createElement('div'),
-        stringWrap = document.createElement('div'),
-        nameTask = document.createElement('div'),
-        nameResponsible = document.createElement('div'),
-        levelPriority = document.createElement('div'),
-        checkBtn = document.createElement('button');
-    let priority = levelPriority.textContent;
+        taskListWrapper.classList.add('taskListWrapper')
+        checkBtn.classList.add('checkBtn')
+        stringWrap.classList.add('stringWrap')
+        line.classList.add('line')
+        nameTask.classList.add('nameTask')
+        levelPriority.classList.add('levelPriority')
+        nameResponsible.classList.add('nameResponsible')
 
-    taskListWrapper.classList.add('taskListWrapper')
-    checkBtn.classList.add('checkBtn')
-    stringWrap.classList.add('stringWrap')
-    line.classList.add('line')
-    nameTask.classList.add('nameTask')
-    levelPriority.classList.add('levelPriority')
-    nameResponsible.classList.add('nameResponsible')
+        nameTask.textContent = item.name
+        nameResponsible.textContent = item.responsible
 
-    nameTask.textContent = item.name
-    nameResponsible.textContent = item.responsible
-
-    levelPriority.textContent = item.priority
-    priority = levelPriority.textContent
-    levelPriority.classList.add(`priority${priority}`)
+        levelPriority.textContent = item.priority
+        priority = levelPriority.textContent
+        levelPriority.classList.add(`priority${priority}`)
 
 
-    if (!taskList.childElementCount) {
-        taskCount = 0;
-    } else {
-        taskCount = taskList.childElementCount
+        if (!taskList.childElementCount) {
+            taskCount = 0;
+        } else {
+            taskCount = taskList.childElementCount
+        }
+        taskListWrapper.id = `${taskCount}`
+        checkBtn.addEventListener('click', () => removeTask(Number(taskListWrapper.id)))
+        taskListWrapper.append(nameTask)
+        taskListWrapper.append(nameResponsible)
+        taskListWrapper.append(levelPriority)
+        taskListWrapper.append(checkBtn)
+
+        stringWrap.append(taskListWrapper)
+        stringWrap.append(line)
+        return stringWrap
     }
-    taskListWrapper.id = `${taskCount}`
-    checkBtn.addEventListener('click', () => removeTask(Number(taskListWrapper.id)))
-    taskListWrapper.append(nameTask)
-    taskListWrapper.append(nameResponsible)
-    taskListWrapper.append(levelPriority)
-    taskListWrapper.append(checkBtn)
-
-    stringWrap.append(taskListWrapper)
-    stringWrap.append(line)
-    return stringWrap
-}
 
 
-const removeTask = (numberTask) => {
-    tasks.splice(numberTask, 1);
-    localStorage.setItem('tasksStorage', JSON.stringify(tasks))
-    renderItems();
+    const removeTask = (numberTask) => {
+        tasks.splice(numberTask, 1);
+        localStorage.setItem('tasksStorage', JSON.stringify(tasks))
+        renderItems();
 
 
-}
+    }
 
-const renderItems = () => {
-    taskList.textContent = ''; // бля не бей я в процессе
-
-
-    tasks.forEach((item) => {
-
-        let stringWrap = createTaskElement(item)
-
-        taskList.append(stringWrap)
+    const renderItems = () => {
+        taskList.textContent = ''; // бля не бей я в процессе
 
 
-    })
-}
+        tasks.forEach((item) => {
 
-const addTask = () => {
-    if (tasks.find((elem) => elem.name === taskInput.value) || !taskInput.value) {
+            let stringWrap = createTaskElement(item)
 
-    } else {
-        tasks.push({
-            name: taskInput.value,
-            responsible: taskResponsible.value,
-            priority: selectPriority.value,
+            taskList.append(stringWrap)
+
+
         })
     }
-    localStorage.setItem('tasksStorage', JSON.stringify(tasks))
+
+    const addTask = () => {
+        if (tasks.find((elem) => elem.name === taskInput.value) || !taskInput.value) {
+
+        } else {
+            tasks.push({
+                name: taskInput.value,
+                responsible: taskResponsible.value,
+                priority: selectPriority.value,
+            })
+        }
+        localStorage.setItem('tasksStorage', JSON.stringify(tasks))
+        renderItems()
+    }
+    saveBtn.addEventListener("click", addTask)
     renderItems()
-}
-saveBtn.addEventListener("click", addTask)
-console.log('kekw')
+})
